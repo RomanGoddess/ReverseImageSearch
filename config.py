74e46b432fa8745a8e9c9d7a1e03a31f85172d21
Ice Queen <53878781+RomanGoddess@users.noreply.github.com>
2020-12-10 14:19:58 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    CLIENT_ID = ""
-    BOT_TOKEN = ""
-    HEROKU_APP_NAME = ""
+    CLIENT_ID = "5bd3ce843b5cfdf"
+    BOT_TOKEN = "1483104859:AAE2qWSdq19KKLbBm8mpaYRGjNYkrhXHPoI"
+    HEROKU_APP_NAME = "reversesearchflixbot"
